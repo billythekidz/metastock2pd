65dcb21f37befdb293cee057a5e70ace19a30e17
billythekidz <nguyenvanthe994@gmail.com>
2022-10-13 13:17:40 +0700

    ! Complete export binance data to multichart

--- binance_data_to_multichart.py
+++ binance_data_to_multichart.py
@@ -14,6 +14,7 @@ for p in listFiles:
     print(pathFile)    
     newFile = open(pathNewFile, 'w', newline='') 
     mcWriter = csv.writer(newFile, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='\\', doublequote=False)    
+    lastestTime = dt.datetime.fromordinal(1)
     with open(pathFile, 'r') as file:
         # reader = pd.read_csv(file)#, skiprows=1)
         reader = csv.reader(file)
@@ -25,19 +26,27 @@ for p in listFiles:
             # price = round(float(row['price']),2)
             # volume = int(float(row['quote_qty']))
             # timestamp = float((row['time'])) / 1000
-            if count == 1: continue
+            if count == 1: continue            
             # print(row)
             # print(".", end ="")
-            tickid = row[0]
+            # tickid = row[0]
             price = round(float(row[1]),2)
             volume = int(float(row[3]))
-            timestamp = float((row[4])) / 1000
-            # print(timestamp)
-            datetime = dt.datetime.utcfromtimestamp(timestamp) #
+            timestamp = float((row[4])) / 1000            
+            # 
+            datetime = dt.datetime.fromtimestamp(timestamp) #
+            if count == 2: lastestTime = datetime
+            if lastestTime >= datetime:
+                # print("before " + str(lastestTime.microsecond))
+                # print("last " + str(lastestTime.timestamp()))
+                datetime = dt.datetime.fromtimestamp(lastestTime.timestamp() + 0.001)
+                # print("after " + str(datetime.microsecond))                
+            lastestTime = datetime
             date = datetime.strftime('%m-%d-%Y') 
-            time = datetime.strftime('%I:%M:%S.%f')[:-3]                                                                       
-            mcRow = [tickid, date, time, price, volume] 
-            mcWriter.writerow(mcRow)
+            time = datetime.strftime('%H:%M:%S.%f')[:-3]                                                                   
+            mcRow = [date, time, price, volume] 
+            mcWriter.writerow(mcRow)            
+            # print(mcRow)     
             #print(fixRow)
             # except:
             #     pass
